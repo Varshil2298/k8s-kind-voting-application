d3d172a5a9a65bb4a0e75ec70cbab9fb36e2c85f
Varshil2298 <desaivarshil5@gmail.com>
2024-10-01 17:17:45 +0530

    new latest changes resuts server.js

--- result/server.js
+++ result/server.js
@@ -2,7 +2,6 @@ var express = require('express'),
     async = require('async'),
     { Pool } = require('pg'),
     cookieParser = require('cookie-parser'),
-    path = require('path'),  // Add path for serving index.html
     app = express(),
     server = require('http').Server(app),
     io = require('socket.io')(server);
@@ -10,7 +9,8 @@ var express = require('express'),
 var port = process.env.PORT || 4000;
 
 io.on('connection', function (socket) {
-  socket.emit('message', { text: 'Welcome!' });
+
+  socket.emit('message', { text : 'Welcome!' });
 
   socket.on('subscribe', function (data) {
     socket.join(data.channel);
@@ -22,16 +22,16 @@ var pool = new Pool({
 });
 
 async.retry(
-  { times: 1000, interval: 1000 },
-  function (callback) {
-    pool.connect(function (err, client, done) {
+  {times: 1000, interval: 1000},
+  function(callback) {
+    pool.connect(function(err, client, done) {
       if (err) {
         console.error("Waiting for db");
       }
       callback(err, client);
     });
   },
-  function (err, client) {
+  function(err, client) {
     if (err) {
       return console.error("Giving up");
     }
@@ -41,8 +41,7 @@ async.retry(
 );
 
 function getVotes(client) {
-  // Query to get votes for all options (including new pairs like a2, b2)
-  client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function (err, result) {
+  client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function(err, result) {
     if (err) {
       console.error("Error performing query: " + err);
     } else {
@@ -50,24 +49,15 @@ function getVotes(client) {
       io.sockets.emit("scores", JSON.stringify(votes));
     }
 
-    setTimeout(function () { getVotes(client); }, 1000);
+    setTimeout(function() {getVotes(client) }, 1000);
   });
 }
 
 function collectVotesFromResult(result) {
-  // Initialize all pairs (extendable as per your HTML)
-  var votes = {
-    a: 0, b: 0,  // For first pair
-    a2: 0, b2: 0,  // For second pair
-    a3: 0, b3: 0,  // For third pair
-    a4: 0, b4: 0   // For fourth pair
-  };
+  var votes = {a: 0, b: 0};
 
-  // Collect votes from the result and map them to correct pair
   result.rows.forEach(function (row) {
-    if (votes.hasOwnProperty(row.vote)) {
-      votes[row.vote] = parseInt(row.count);
-    }
+    votes[row.vote] = parseInt(row.count);
   });
 
   return votes;
@@ -78,7 +68,7 @@ app.use(express.urlencoded());
 app.use(express.static(__dirname + '/views'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.resolve(__dirname + '/views/index.html'));  // Use path.resolve to correctly serve index.html
+  res.sendFile(path.resolve(__dirname + '/views/index.html'));
 });
 
 server.listen(port, function () {
